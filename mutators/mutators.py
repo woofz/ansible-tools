fdcacebe8e83e8d5c519bd2478e2475c6ac7cb13
Danilo Aliberti <danilo.test@gmail.com>
2021-09-13 11:43:14 +0200

    Initial commit

---
+++ mutators/mutators.py
@@ -0,0 +1,104 @@
+import random
+
+
+def mutate(module: dict, module_used: str):
+    """ Mutate a module and return it.
+
+    Example:
+    -------
+    service_params = {
+        'state': ['started', 'stopped', 'reloaded', 'restarted'],
+        'enabled': ['no', 'yes'],
+    }
+
+    actual_service = {
+        'name': 'ciao',
+        'state': 'reloaded',
+        'enabled': 'no'
+    }
+
+    mutated_module = mutate(actual_service, service_params)
+    print(mutated_module)
+
+
+    Return
+    ------
+        mutated_module: dict - the mutated module
+        None: if none of the module's parameters passed in input can be mutated (i.e, is not in module_params)
+    """
+    # module_list = list(tasks_df['mod_keys_found_string'].unique())  # List of modules
+    # module_used = list(set(module_list) & set(module))[0]  # Used module
+    module_params = module_parameters[module_used]
+
+    mutable_module_params = set(module.keys()).intersection(set(module_params.keys()))
+
+    if not mutable_module_params:
+        return module
+
+    param_to_mutate = random.choice(list(mutable_module_params))
+    mutable_param_values = module_params[param_to_mutate]
+    #mutable_param_values.remove(module[param_to_mutate])
+    mutated_value = random.choice(list(mutable_param_values))
+
+    mutated_module = module
+    mutated_module[param_to_mutate] = mutated_value
+
+    print('Mutated')
+    return mutated_module
+
+
+module_parameters = {
+    'service': {
+        'state': ['started', 'stopped', 'reloaded', 'restarted'],
+        'enabled': [False, True],
+    },
+
+    'set_fact': {
+        'cacheable': [False, True],
+    },
+
+    'shell': {
+        'stdin_add_newline ': [False, True],
+    },
+
+    'command': {
+        'stdin_add_newline ': [False, True],
+        'strip_empty_ends': [False, True],
+        'warn': [False, True],
+    },
+
+    'template': {
+        'backup': [False, True],
+        'follow': [False, True],
+        'force': [True, False],
+        'lstrip_blocks': [True, False],
+        'newline_sequence': [True, False],
+        'trim_blocks': [True, False],
+        'unsafe_writes': [True, False],
+    },
+
+    'file': {
+        'follow': [True, False],
+        'force': [True, False],
+        'recurse': [True, False],
+        'unsafe_writes': [True, False],
+        'state': ['absent', 'directory', 'file', 'hard', 'link', 'touch'],
+    },
+
+    'copy': {
+        'backup': [True, False],
+        'decrypt': [True, False],
+        'follow': [True, False],
+        'force': [True, False],
+        'local_follow': [True, False],
+        'remote_src': [True, False],
+        'unsafe_writes': [True, False],
+    },
+
+    'gather_facts': {
+        'parallel': [True, False],
+    },
+
+    'fail': {},
+    'debug': {},
+}
---
+++ utils/get_parameters.py
@@ -0,0 +1,59 @@
+import requests
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
+import json
+
+
+def get_module_parameters(module_name):
+    """ Given module name, this function retrieves module's parameters from official ansible documentation """
+
+    url = f'https://docs.ansible.com/ansible/latest/collections/ansible/builtin/{module_name}_module.html#' \
+          f'{module_name}-module'
+    content = requests.get(url).content.decode()
+    params = list(re.findall(r'parameter-(\w+)\"', content))
+    params = list(dict.fromkeys(params))
+    param_values = get_parameters_values(url)
+    param_zip = zip(params, param_values)
+    return dict(param_zip)
+
+
+def get_parameters_values(url):
+    html_content = requests.get(url).text
+
+    soup = BeautifulSoup(html_content, "lxml")
+    gdp = soup.find_all("table", attrs={"class": "documentation-table"})
+    table1 = gdp[0]
+
+    # the head will form our column names
+    body = table1.find_all("tr")
+
+    head = body[0]  # 0th item is the header row
+    body_rows = body[1:]  # All other items becomes the rest of the rows
+
+    headings = []
+    for item in head.find_all("th"):  # loop through all th elements
+        # convert the th elements to text and strip "\n"
+        item = item.text.rstrip("\n")
+        # append the clean column name to headings
+        headings.append(item)
+
+    all_rows = []  # will be a list for list for all rows
+    for row_num in range(len(body_rows)):  # A row at a time
+        row = []  # this will old entries for one row
+        for row_item in body_rows[row_num].find_all("td"):  # loop through all row entries
+            aa = re.sub("(\xa0)|(\n)|,", " ", row_item.text)
+            row.append(aa)
+        # append one row to all_rows
+        all_rows.append(row)
+
+    # We can now use the data on all_rows and headings to make a table
+    # all_rows becomes our data and headings the column names
+    df = pd.DataFrame(data=all_rows, columns=headings)
+    values_list = []
+
+    for item in df['Choices/Defaults']:
+        item = item.replace('←', '').replace('Choices: ', '').replace('  ', ' ')
+        values_list.append(item.split())
+
+    return values_list
